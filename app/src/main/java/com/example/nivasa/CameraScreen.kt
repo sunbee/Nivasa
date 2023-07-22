9a0c987a212a4db2deda0a4502aa7ac1cf9f66e3
sunbee <sanjay.bhatikar@gmail.com>
2023-07-21 22:36:17 -0500

    First payload

---
+++ app/src/main/java/com/example/nivasa/CameraScreen.kt
@@ -0,0 +1,59 @@
+package com.example.nivasa
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.camera.core.CameraProvider
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+
+@Composable
+fun CameraPreview(
+    modifier: Modifier,
+    hasCameraPermission: Boolean,
+    cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+) {
+    val context = LocalContext.current
+    val lifeCycleOwner = LocalLifecycleOwner.current
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (hasCameraPermission) {
+
+            AndroidView(
+                factory = { context ->
+                    val previewView = PreviewView(context)
+                    val preview = Preview.Builder().build()
+                    val selector = CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
+                    preview.setSurfaceProvider(previewView.surfaceProvider)
+                    try {
+
+                        cameraProviderFuture.get().bindToLifecycle(
+                            lifeCycleOwner,
+                            selector,
+                            preview
+                        )
+                    } catch(e: Exception) {
+                        e.printStackTrace()
+                    }
+                    previewView
+                }
+            )
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/nivasa/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.nivasa
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.example.nivasa.ui.theme.NivasaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NivasaTheme {
+                val context = LocalContext.current
+                val lifecycleOwner = LocalLifecycleOwner.current
+                val cameraProviderFuture = remember {
+                    ProcessCameraProvider.getInstance(context)
+                }
+                var hasCameraPermission by remember {
+                    mutableStateOf(
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    )
+                }
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = { granted ->
+                        hasCameraPermission = granted
+                    })
+                LaunchedEffect(key1 = true, block = {
+                    launcher.launch(Manifest.permission.CAMERA)
+                })
+                CameraPreview(modifier = Modifier,
+                    hasCameraPermission = hasCameraPermission,
+                    cameraProviderFuture = cameraProviderFuture)
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NivasaTheme {
+        Greeting("Android")
+    }
+}

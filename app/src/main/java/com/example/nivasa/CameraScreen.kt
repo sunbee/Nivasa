ce89cd2cc80f6c8c00220f82e261655ccaf0a3ef
sunbee <sanjay.bhatikar@gmail.com>
2023-07-22 13:47:55 -0500

    Refactored and expanded comments, added button (dummy) to take snap.

--- app/src/main/java/com/example/nivasa/CameraScreen.kt
+++ app/src/main/java/com/example/nivasa/CameraScreen.kt
@@ -1,18 +1,28 @@
 package com.example.nivasa
 
+import android.content.Context
+import android.os.Environment
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
 
 @Composable
 fun CameraPreview(
@@ -28,11 +38,34 @@ fun CameraPreview(
         if (hasCameraPermission) {
 
             val cameraProviderFuture = remember {
+                /*
+                * CameraProviderFuture: The cameraProviderFuture is a ListenableFuture
+                * that represents the asynchronous operation of obtaining the ProcessCameraProvider.
+                * The future is used to retrieve the ProcessCameraProvider,
+                * to use to bind the camera lifecycle.
+                * */
                 ProcessCameraProvider.getInstance(context)
             }  // Must be in composable scope
 
+            /*
+            * Handle the camera preview and binding the camera lifecycle using the CameraX library.
+            * The AndroidView composable allows integration of the PreviewView
+            * into the Jetpack Compose hierarchy seamlessly, and the Column composable
+            * provides a neat and simple way to organize the UI elements.
+            * */
             AndroidView(
+                /*
+                * AndroidView: The AndroidView composable is used to embed native Android views,
+                * such as PreviewView, into Jetpack Compose. Inside the factory parameter of AndroidView,
+                * create the PreviewView, set up the camera preview, and bind the camera lifecycle
+                * using bindToLifecycle.*/
                 factory = { context ->
+                    /*
+                    * PreviewView: The PreviewView is provided by the CameraX library
+                    * and is used to display the camera preview on the screen.
+                    * Set up the Preview use case with preview.setSurfaceProvider
+                    * to show the camera feed within the PreviewView.
+                    * */
                     val previewView = PreviewView(context)
                     val preview = Preview.Builder().build()
                     val selector = CameraSelector.Builder()
@@ -42,6 +75,12 @@ fun CameraPreview(
 
                     cameraProviderFuture.get()?.unbindAll()
                     try {
+                        /*
+                        * try-catch: Properly handle any exceptions that might occur
+                        * during the camera binding process using a try-catch block.
+                        * If an exception is thrown, it is caught, Print the stack trace
+                        * to diagnose any potential issues.
+                        * */
                         cameraProviderFuture.get().bindToLifecycle(
                             lifeCycleOwner,
                             selector,
@@ -54,5 +93,36 @@ fun CameraPreview(
                 }  // end FACTORY
             )  // end ANDROIDVIEW
         }  // end IF CAMERAHASPERMISSION
+        // Button for capturing photos
+        Button(
+            onClick = { /**/ }, // Call the capturePhoto function on button click
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+        ) {
+            Text(text = "Capture Photo")
+        }
     }  // end COLUMN
+}
+
+private fun captureSnap(context: Context, imageCapture: ImageCapture) {
+    val outputDirectory = getOutputDirectory(context)
+
+}
+
+private fun getOutputDirectory(context: Context): File {
+    /*
+    * Use ContextCompat.getExternalFilesDirs(context, Environment.DIRECTORY_PICTURES)
+    * to get an array of directories where the app can save files,
+    * specifically in the DIRECTORY_PICTURES. We then use firstOrNull
+    * to find the first non-null directory that is mounted and available.
+    * If mediaDir is not null, return it as the output directory.
+    * Otherwise, we fallback to using the app's internal files directory (context.filesDir)
+    * if there is no mounted external storage available.
+    * */
+    val mediaDirectories = ContextCompat.getExternalFilesDirs(context, Environment.DIRECTORY_PICTURES)
+    val mediaDirectory = mediaDirectories.firstOrNull() {
+        it != null && Environment.MEDIA_MOUNTED == Environment.getExternalStorageState(it)
+    }
+    return mediaDirectory ?: context.filesDir
 }

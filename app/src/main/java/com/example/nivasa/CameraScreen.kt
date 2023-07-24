6949f4c08f9152c34d46c04784e2b2327f9072a8
sunbee <sanjay.bhatikar@gmail.com>
2023-07-23 19:11:45 -0500

    Tidied up code.

--- app/src/main/java/com/example/nivasa/CameraScreen.kt
+++ app/src/main/java/com/example/nivasa/CameraScreen.kt
@@ -20,8 +20,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -29,12 +27,13 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
-import com.google.common.util.concurrent.ListenableFuture
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Locale
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun CameraPreview(
     modifier: Modifier,

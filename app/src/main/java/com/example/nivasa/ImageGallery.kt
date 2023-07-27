2012295bdb35be7809562b8e7bd33280a94680bf
sunbee <sanjay.bhatikar@gmail.com>
2023-07-26 22:04:30 -0500

    Tidied up code.

--- app/src/main/java/com/example/nivasa/ImageGallery.kt
+++ app/src/main/java/com/example/nivasa/ImageGallery.kt
@@ -1,6 +1,5 @@
 package com.example.nivasa
 
-import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
